ec21aed8c54c656b59734114174687ebd3b78a4a
avnish986 <avnishgupta986@gmail.com>
2020-11-06 16:49:54 +0530

    [Avnish]Added:calculating employee wage using while loop

--- EmpWageCalculator.js
+++ EmpWageCalculator.js
@@ -1,4 +1,4 @@
-//UC4 For Loop
+//UC5 While Loop
 {
     const IS_PART_TIME = 1;
     const IS_Full_TIME = 2;
@@ -6,6 +6,7 @@
     const FULL_TIME_HOURS = 8;
     const WAGE_PER_HOUR=20;
     const NUM_OF_WORKING_DAYS=20;
+    const MAX_HRS_IN_MONTH=160;
     function getEmpHrs(empCheck){
     switch(empCheck){
         case IS_PART_TIME:
@@ -20,10 +21,12 @@
         }
     }
     let totalEmpHrs=0;
-    for(let day =0; day<NUM_OF_WORKING_DAYS;day++){
-        let empCheck=Math.floor(Math.random() * 10)%3;
+    let totalWorkingDays=0;
+    while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS){
+        totalWorkingDays++;
+        let empCheck = Math.floor(Math.random() * 10)%3;
         totalEmpHrs+=getEmpHrs(empCheck);
     }
     let empWage = totalEmpHrs*WAGE_PER_HOUR;
-    console.log("Hour: "+totalEmpHrs+" Emp Wage "+empWage);
+    console.log("Total Days: "+totalWorkingDays + " total Hrs: "+totalEmpHrs+" Emp Wage "+empWage);
 }
